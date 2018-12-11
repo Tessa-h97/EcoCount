f089ab9e98f9a02ab6fdd9b7baab26adb38c3f57
tessahaschtschek <it15157@lehre.dhbw-stuttgart.de>
2018-12-11 20:10:31 +0100

    ADD: Wieder in Swift und Xcode einlernen

---
+++ EcoCount/MainMenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainMenuViewController.swift
+//  EcoCount
+//
+//  Created by Tessa Haschtschek on 11.12.18.
+//  Copyright © 2018 Tessa Haschtschek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainMenuViewController : UIViewController {
+    override func viewDidLoad() {
+        //Yay
+    }
+}
